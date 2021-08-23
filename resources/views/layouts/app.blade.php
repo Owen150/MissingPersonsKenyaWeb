390bca000d36de8abb577b07083887da4cb77f7e
Michael Owen Oduor <34193283+Owen150@users.noreply.github.com>
2021-08-23 19:12:24 +0300

    Updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Missing Persons Kenya') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -35,7 +35,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
+                        <a class="nav-link" href="{{route('welcome')}}">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{route('about')}}">About</a>
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -7,10 +7,8 @@
     <title>Todos</title>
 </head>
 <body>
-    <h1>LANDING PAGE</h1>
     @foreach($crs as $cr)
-        <p>{{ $cr->name_of_missing_person }}</p>
-       
+        <p>{{ $cr->name_of_missing_person }}</p>  
     @endforeach
 </body>
 </html>
