1cb08e35477e40d7d38c30915b07d962868cb238
StacyChebet <staceychebet23@gmail.com>
2021-08-25 07:21:31 +0300

    Made changes to the top navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,15 +4,70 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <!-- Fonts -->
+
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="{{ url('./css/index.css') }}" />
+        <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.css') }}"/>
+        <link rel="stylesheet" type="text/css" href="{{ url('css/index.css') }}" />
         <title>missingPersonsApp</title>
     </head>
 
     <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+            <div class="container">
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/images/Logo.jpg" style="height:30px; border-right:1px solid #333;" class="pr-3"></div>
+                    <div class="pl-3 pt-1">Missing Persons Kenya</div>
+                </a>
+               
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <!-- Left Side Of Navbar -->
+                <div class="collapse navbar-collapse" id="navbarNav">                    
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('LOGIN') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('REGISTER') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+            <!-- @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
@@ -24,7 +79,7 @@
                         @endif
                     @endauth
                 </div>
-            @endif
+            @endif -->
             <div class="contentWrapper">
                 <div class="content">
                     <p class="">Missing Persons Kenya helps you:</br>+ View missing persons reports<br/>+ Report missing persons
